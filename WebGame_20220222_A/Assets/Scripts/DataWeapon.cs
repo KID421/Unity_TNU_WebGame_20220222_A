41ed6c470de843622d0d0e4ebd1d7ab7a780d868
KID <50666207+KID421@users.noreply.github.com>
2022-03-22 12:00:52 +0800

    完成角色控制器並添加武器資料

---
+++ WebGame_20220222_A/Assets/Scripts/DataWeapon.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+
+namespace KID
+{
+    /// <summary>
+    /// 武器資料
+    /// 1. 飛行速度
+    /// 2. 攻擊力
+    /// 3. 數量
+    /// 4. 數量上限
+    /// 5. 生成位置
+    /// </summary>
+    [CreateAssetMenu(menuName = "KID/Data Weapon", fileName = "Data Weapon")]
+    public class DataWeapon : ScriptableObject
+    {
+
+    }
+}
